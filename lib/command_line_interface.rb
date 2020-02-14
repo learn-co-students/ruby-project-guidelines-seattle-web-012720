15c60c3a6a1af12539821b0d548b3ff76f70a61c
github username <github email address>
2020-02-14 09:31:49 -0800

    Done.

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -225,13 +225,17 @@ class CommandLineInterface
       print "Great, that patient is in our area, you can proceed with the new order\n\n"
       @zip = zip
     else
-      print "Looks like area code "
-      print zip
-      print " isnt in our area\n"
-      print "Please call the local Track-O2 office in "
-      print zip
-      print " and fax them this order.\n"
-      #   start
+      if zip == 0 || zip.digits.count > 5
+        print "That was not a valid zip code. please try again\n"
+        check_zip
+      else
+        print "Looks like area code "
+        print zip
+        print " isnt in our area\n"
+        print "Please call the local Track-O2 office in "
+        print zip
+        print " and fax them this order.\n"
+      end
     end
   end
 
@@ -245,17 +249,17 @@ class CommandLineInterface
     end
     assigned_csr_id = gets.chomp.to_i
     if assigned_csr_id > CustomerServiceRep.all.length
+      print "Please try again. \n"
+      assign_csr
+    else
+      if assigned_csr_id < 1
         print "Please try again. \n"
         assign_csr
       else
-        if assigned_csr_id < 1
-          print "Please try again. \n"
-          assign_csr
-        else
-    @assigned_csr_id = assigned_csr_id
-    print get_name_csr(assigned_csr_id)
-    print " is creating this order.\n\n"
-        end
+        @assigned_csr_id = assigned_csr_id
+        print get_name_csr(assigned_csr_id)
+        print " is creating this order.\n\n"
+      end
     end
   end
 
@@ -269,19 +273,19 @@ class CommandLineInterface
     end
     assigned_sr_id = gets.chomp.to_i
     if assigned_sr_id > ServiceRep.all.length
+      print "Please try again. \n"
+      assign_sr
+    else
+      if assigned_sr_id < 1
         print "Please try again. \n"
         assign_sr
       else
-        if assigned_sr_id < 1
-          print "Please try again. \n"
-          assign_sr
-        else
-            @assigned_sr_id = assigned_sr_id
-            print get_name_sr(assigned_sr_id)
-            print " will be delivering/setting up this order.\n\n"
-         end
-        end
+        @assigned_sr_id = assigned_sr_id
+        print get_name_sr(assigned_sr_id)
+        print " will be delivering/setting up this order.\n\n"
+      end
     end
+  end
 
   def order_type
     order_types = ["24-hour oxygen", "nocturnal oxygen", "nebulizer device"]
@@ -296,16 +300,16 @@ class CommandLineInterface
     end
     selection = gets.chomp.to_i
     if selection > order_types.length
+      print "Please try again. \n"
+      order_type
+    else
+      if selection < 1
         print "Please try again. \n"
         order_type
       else
-        if selection < 1
-          print "Please try again. \n"
-          order_type
-        else
-    index = (selection - 1)
-    @order_type = order_types[index]
-        end
+        index = (selection - 1)
+        @order_type = order_types[index]
+      end
     end
   end
 
@@ -334,8 +338,8 @@ class CommandLineInterface
     elsif yn == "N"
       assign_csr
     else
-        print "please try again"
-        confirm
+      print "please try again"
+      confirm
     end
     print "Is " + get_name_sr(@assigned_sr_id) + " delivering this order?"
     yn = gets.chomp
@@ -344,8 +348,8 @@ class CommandLineInterface
     elsif yn == "N"
       assign_sr
     else
-        print "please try again"
-        confirm
+      print "please try again"
+      confirm
     end
     print "Is this an order for " + @order_type + "?"
     yn = gets.chomp
@@ -354,8 +358,8 @@ class CommandLineInterface
     elsif yn == "N"
       order_type
     else
-        print "please try again"
-        confirm
+      print "please try again"
+      confirm
     end
     # binding.pry
     print "For special instructions, you said:" + @spuckts
@@ -365,8 +369,8 @@ class CommandLineInterface
     elsif yn == "N"
       get_notes
     else
-        print "please try again"
-        confirm
+      print "please try again"
+      confirm
     end
   end
 
